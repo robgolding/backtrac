fd58b832a29e11bc1dfda4ec3cd33ed0d9d49a44
Rob Golding <rob@robgolding.com>
2010-07-09 19:45:28 +0100

    Fixed bug in RequireLoginMiddleware (allows debug-toolbar to work without logging in)

--- cecil/apps/core/middleware.py
+++ cecil/apps/core/middleware.py
@@ -3,14 +3,17 @@ from django.contrib.auth.views import login
 from django.http import HttpResponseRedirect
 
 class RequireLoginMiddleware(object):
-    def __init__(self):
-	    self.login_url = getattr(settings, 'LOGIN_URL', '/accounts/login/')
-	    self.static_url = getattr(settings, 'STATIC_URL', '/static/')
-	
-    def process_request(self, request):
-        if request.user.is_anonymous():
-        	if request.path != self.login_url and not request.path.startswith(self.static_url):
-		        if request.POST:
-		            return login(request)
-		        else:
-		            return HttpResponseRedirect('%s?next=%s' % (self.login_url, request.path))
+
+	EXCLUDED_URLS = (
+		'/accounts/login/',
+		'/__debug__/',
+		settings.STATIC_URL,
+	)
+
+	def process_request(self, request):
+		if request.user.is_anonymous():
+			if not request.path.startswith(self.EXCLUDED_URLS):
+				if request.POST:
+					return login(request)
+				else:
+					return HttpResponseRedirect('%s?next=%s' % (self.login_url, request.path))
