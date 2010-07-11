d9abc2d5cad179be2d1b25de09926e5b70d83481
Rob Golding <rob@robgolding.com>
2010-07-11 18:30:16 +0100

    Added secret_key field to host model

--- cecil/apps/hosts/models.py
+++ cecil/apps/hosts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Host(models.Model):
 	hostname = models.CharField(max_length=255, unique=True, db_index=True)
+	secret_key = models.CharField(max_length=255)
 	
 	@models.permalink
 	def get_absolute_url(self):
