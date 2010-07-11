efff028d773db3becc1a9334223045325e44da8b
Rob Golding <rob@robgolding.com>
2010-07-11 18:30:28 +0100

    Added authentication to API based on hostname and secret_key

--- cecil/api/v1/urls.py
+++ cecil/api/v1/urls.py
@@ -4,7 +4,14 @@ from piston.authentication import HttpBasicAuthentication
 
 from handlers import BackupHandler
 
-auth = HttpBasicAuthentication(realm="Backtrac API")
+def host_auth(username, password):
+	from cecil.apps.hosts.models import Host
+	try:
+		return Host.objects.get(hostname=username, secret_key=password)
+	except Host.DoesNotExist:
+		return None
+
+auth = HttpBasicAuthentication(realm="Backtrac API", auth_func=host_auth)
 ad = { 'authentication': auth }
 
 backup_handler = Resource(BackupHandler, **ad)
