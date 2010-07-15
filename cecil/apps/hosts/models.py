a5c43cfe57cf7252e7480f0eb065e134123f637a
Rob Golding <rob@robgolding.com>
2010-07-15 19:45:26 +0100

    Added host status and styled host list with icons

--- cecil/apps/hosts/models.py
+++ cecil/apps/hosts/models.py
@@ -8,5 +8,24 @@ class Host(models.Model):
 	def get_absolute_url(self):
 		return ('hosts_host_detail', [self.id])
 	
+	def is_backing_up(self):
+		return any([ b.is_running() for b in self.backups.all()])
+	
+	def get_last_completed_result(self):
+		Result = models.get_model('backups', 'Result')
+		try:
+			return Result.objects.filter(backup__client=self, finished_at__isnull=False).latest()
+		except Result.DoesNotExist:
+			return None
+	
+	def get_status(self):
+		if self.is_backing_up():
+			return 'backup running'
+		r = self.get_last_completed_result()
+		if r and not r.successful:
+			return 'error'
+		else:
+			return 'idle'
+	
 	def __unicode__(self):
 		return self.hostname
