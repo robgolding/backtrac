550d49a4ca53736d478d9ed4d0665a6b4c9d94e3
Rob Golding <rob@robgolding.com>
2010-11-09 14:41:23 +0000

    Add Directory model to catalog and recursive code to compute full path to file

--- backtrac/apps/catalog/admin.py
+++ backtrac/apps/catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from models import File, FileVersion
+from models import Directory, File, FileVersion
 
+admin.site.register(Directory)
 admin.site.register(File)
 admin.site.register(FileVersion)
--- backtrac/apps/catalog/models.py
+++ backtrac/apps/catalog/models.py
@@ -1,17 +1,40 @@
 from django.db import models
 
+class Directory(models.Model):
+    parent = models.ForeignKey('self', null=True, blank=True)
+    name = models.CharField(max_length=255)
+
+    def _get_path(self):
+        if self.parent is not None:
+            return self.parent.path + self.name + '/'
+        return '/%s/' % self.name
+    path = property(_get_path)
+
+    def __unicode__(self):
+        return self.path
+
+    class Meta:
+        verbose_name_plural = 'directories'
+
 class File(models.Model):
     client = models.ForeignKey('clients.Client', related_name='files')
-    path = models.CharField(max_length=255)
+    directory = models.ForeignKey(Directory, null=True, blank=True)
+    name = models.CharField(max_length=255)
+
+    def _get_path(self):
+        if self.directory is not None:
+            return self.directory.path + self.name
+        return '/%s' % self.name
+    path = property(_get_path)
 
     def __unicode__(self):
         return self.path
 
 class FileVersion(models.Model):
-    id = models.CharField(max_length=36, primary_key=True)
+    id = models.CharField('ID', max_length=36, primary_key=True)
     file = models.ForeignKey(File, related_name='versions')
     backed_up_at = models.DateTimeField(auto_now_add=True)
-    mtime = models.IntegerField()
+    mtime = models.IntegerField('Modified time')
     size = models.BigIntegerField()
 
     class Meta:
