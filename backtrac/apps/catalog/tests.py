a4c4deebd2eafb9e9935967313e33ea9d6235fda
Rob Golding <rob@robgolding.com>
2010-11-30 14:59:05 +0000

    Add ItemGetNameTest to catalog tests and tidy up existing LatestVersionTest

--- backtrac/apps/catalog/tests.py
+++ backtrac/apps/catalog/tests.py
@@ -1,25 +1,66 @@
 """
 Tests for the `catalog' application.
 """
-import uuid
+import uuid, random
 
 from django.test import TestCase
 
 from backtrac.apps.clients.models import Client
 from models import Item, Version
 
-class LatestVersionTest(TestCase):
+class ItemGetNameTest(TestCase):
     def setUp(self):
-        uid = str(uuid.uuid4())
+        """
+        Create a single client and two items within that client - a file and
+        a directory.
+        """
         c = Client.objects.create(hostname='testclient', secret_key='')
-        i = Item.objects.create(client=c, name='test', type='f')
-        v = Version.objects.create(id=uid, item=i, mtime=12345, size=12345)
-        self.item = i
-        self.version = v
+        self.item_file = Item.objects.create(client=c, name='file', type='f')
+        self.item_dir = Item.objects.create(client=c, name='dir', type='d')
+
+    def test_dir_get_name(self):
+        """Test that the directory name ends in a slash"""
+        self.failUnless(self.item_dir.get_name().endswith('/'))
+
+    def test_file_get_name(self):
+        """Test that the file name doesn't end in a slash"""
+        self.failIf(self.item_file.get_name().endswith('/'))
+
+class LatestVersionTest(TestCase):
+    def _add_version_to_item(self, item):
+        """
+        Stick another version onto the given item with a new UUID, and a
+        random mtime and size - then return it.
+        """
+        uid = str(uuid.uuid4())
+        mtime = random.randrange(123, 12345)
+        size = random.randrange(123, 12345)
+        return Version.objects.create(id=uid, item=item,
+                                   mtime=23456, size=23456)
+
+    def setUp(self):
+        """
+        Create a single client and a single item within that client. Then, add
+        10 new versions to that item.
+        """
+        self.client = Client.objects.create(hostname='test',
+                                            secret_key='')
+        self.item = Item.objects.create(client=self.client,
+                                        name='test',
+                                        type='f')
+        for i in range(10):
+            self.version = self._add_version_to_item(self.item)
 
     def test_latest_version(self):
         """
-        Tests that the latest version is correct.
+        Test that the returned 'latest' version is the last one that was
+        added.
         """
         self.failUnlessEqual(self.item.get_last_modified_version().id,
                              self.version.id)
+        self.failUnlessEqual(self.item.get_last_modified_version().item,
+                             self.version.item)
+        self.failUnlessEqual(self.item.get_last_modified_version().mtime,
+                             self.version.mtime)
+        self.failUnlessEqual(self.item.get_last_modified_version().size,
+                             self.version.size)
