56fe47a9296325ea42040432d3cb892cc647f7d5
Rob Golding <rob@robgolding.com>
2010-11-30 20:33:15 +0000

    Add test settings and logic to import them when running manage.py test

--- backtrac/settings/__init__.py
+++ backtrac/settings/__init__.py
@@ -1,6 +1,14 @@
+import sys
+
 from default import *
 
 try:
     from local import *
 except ImportError:
     pass
+
+if 'test' in sys.argv:
+    try:
+        from test import *
+    except ImportError:
+        pass
---
+++ backtrac/settings/test.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
