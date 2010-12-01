4ec750a11cc79fa70a2645d96e347c0e67325e06
Rob Golding <rob@robgolding.com>
2010-12-01 21:53:43 +0000

    Add first test (create_client) to clients app

--- backtrac/apps/clients/tests.py
+++ backtrac/apps/clients/tests.py
@@ -1,23 +1,38 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+Tests for the `clients' application.
 """
-
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from models import Client
+
+class CreateClientTest(TestCase):
+    def setUp(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Create a test user and record some test fields for use later on.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        User.objects.create_user('test', 'test@test.com', 'test')
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+        self.hostname = 'test'
+        self.secret_key = 'secret'
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_create_client_view(self):
+        """
+        Login using the test user, then POST to the create_client view. Check
+        that the response code is OK and that the client has been created
+        correctly.
+        """
+        self.client.login(username='test', password='test')
 
+        response = self.client.post('/clients/create/', {
+            'hostname': self.hostname,
+            'secret_key': self.secret_key,
+            'filepaths-TOTAL_FORMS': 0,
+            'filepaths-INITIAL_FORMS': 0,
+            'filepaths-MAX_NUM_FORMS': 0,
+        }, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.context['object'].hostname, self.hostname)
+        self.assertEqual(response.context['object'].secret_key, self.secret_key)
