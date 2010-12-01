aabe461afd1e0b9a3aa3ecfc1ab2a1cb16f0fb31
Rob Golding <rob@robgolding.com>
2010-12-01 22:08:20 +0000

    Add update_client view test to clients app

--- backtrac/apps/clients/tests.py
+++ backtrac/apps/clients/tests.py
@@ -36,3 +36,38 @@ class CreateClientTest(TestCase):
 
         self.assertEqual(response.context['object'].hostname, self.hostname)
         self.assertEqual(response.context['object'].secret_key, self.secret_key)
+
+class UpdateClientTest(TestCase):
+    def setUp(self):
+        """
+        Create a test user, an initial Client object to modify in the test, and
+        some fields to modify it with.
+        """
+        User.objects.create_user('test', 'test@test.com', 'test')
+
+        self.client_obj = Client.objects.create(hostname='test',
+                                            secret_key='secret')
+        self.hostname = 'newhostname'
+        self.secret_key = 'new_secret_key'
+
+    def test_create_client_view(self):
+        """
+        Login using the test user, then POST to the update_client view. Check
+        that the response code is OK and that the client has been updated
+        correctly.
+        """
+        self.client.login(username='test', password='test')
+
+        response = self.client.post('/clients/%d/update/' %
+                                    self.client_obj.id, {
+                                        'hostname': self.hostname,
+                                        'secret_key': self.secret_key,
+                                        'filepaths-TOTAL_FORMS': 0,
+                                        'filepaths-INITIAL_FORMS': 0,
+                                        'filepaths-MAX_NUM_FORMS': 0,
+                                    }, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.context['object'].hostname, self.hostname)
+        self.assertEqual(response.context['object'].secret_key, self.secret_key)
