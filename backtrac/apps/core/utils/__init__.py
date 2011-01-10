cee02d4665a49f84164feed2585c06ac147fd73b
Rob Golding <rob@robgolding.com>
2011-01-10 21:28:46 +0000

    Abstract the run_in_process method for querying server and rewrite status view

--- backtrac/apps/core/utils/__init__.py
+++ backtrac/apps/core/utils/__init__.py
@@ -6,19 +6,32 @@ def makedirs(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def check_daemon_status(q):
+def run_in_process(func, args=[]):
+    from multiprocessing import Process, Queue
+
+    q = Queue()
+    args = [q] + args
+    p = Process(target=func, args=args)
+    p.start()
+    p.join()
+    result = q.get()
+    if isinstance(result, Exception):
+        raise result
+    return result
+
+def get_server_status(q):
     from twisted import cred
     from twisted.spread import pb
     from twisted.internet import reactor
 
     from backtrac.client import BackupClient
 
-    def _connected(perspective):
-        q.put(True)
-        reactor.stop()
-
     def _error(error):
         q.put(False)
+        reactor.stop()
+
+    def _connected(perspective):
+        q.put(True)
         reactor.stop()
 
     client = BackupClient(server='localhost', secret_key=settings.SECRET_KEY)
@@ -36,3 +49,43 @@ def check_daemon_status(q):
     d.addErrback(_error)
 
     reactor.run()
+
+def call_remote_method(q, method):
+    from twisted import cred
+    from twisted.spread import pb
+    from twisted.internet import reactor
+
+    from backtrac.client import BackupClient
+
+    def _error(error):
+        q.put(error)
+        reactor.stop()
+
+    def _connected(perspective):
+        perspective.callRemote(method).addCallbacks(_result, _error)
+
+    def _result(result):
+        q.put(result)
+        reactor.stop()
+
+    client = BackupClient(server='localhost', secret_key=settings.SECRET_KEY)
+
+    factory = pb.PBClientFactory()
+    reactor.connectTCP('localhost', 8123, factory)
+    d = factory.login(
+        cred.credentials.UsernamePassword(
+            'localhost',
+            settings.SECRET_KEY
+        ),
+        client=client
+    )
+    d.addCallback(_connected)
+    d.addErrback(_error)
+
+    reactor.run()
+
+def num_clients():
+    return run_in_process(call_remote_method, ['num_clients',])
+
+def server_status():
+    return run_in_process(get_server_status)
--- backtrac/apps/core/views.py
+++ backtrac/apps/core/views.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
 
-from utils import check_daemon_status
+from utils import server_status
 
 @login_required
 def index(request):
@@ -16,11 +16,6 @@ def dashboard(request, *args, **kwargs):
 
 @login_required
 def status(request):
-    from multiprocessing import Process, Queue
-
-    q = Queue()
-    p = Process(target=check_daemon_status, args=[q,])
-    p.start()
-    p.join()
-    r, s = ('running', 200) if q.get() else ('not running', 412)
+    status = server_status()
+    r, s = ('running', 200) if status else ('not running', 412)
     return HttpResponse(r, status=s)
