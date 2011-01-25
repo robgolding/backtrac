789e2035e64c30feb12324375d81b04e54c55b7b
Rob Golding <rob@robgolding.com>
2011-01-25 23:12:04 +0000

    Add some basic stats to the dashboard

--- backtrac/apps/core/views.py
+++ backtrac/apps/core/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
+from django.db.models import Sum
+
+from backtrac.apps.catalog.models import Item, Version
 
 from utils import server_status
 
@@ -11,7 +14,28 @@ def index(request):
 
 @login_required
 def dashboard(request, *args, **kwargs):
-    kwargs.update({'template': 'dashboard.html'})
+    import os
+    from django.conf import settings
+    stat = os.statvfs(settings.BACKTRAC_BACKUP_ROOT)
+    size = stat.f_blocks * stat.f_frsize
+    used = size - stat.f_bavail * stat.f_frsize
+    used_pc = int(float(used) / float(size) * 100)
+    catalog_size = Version.objects.aggregate(size=Sum('size'))['size']
+
+    stats = {
+        'size': size,
+        'used': used,
+        'used_pc': used_pc,
+        'catalog_size': catalog_size,
+    }
+
+    kwargs.update({
+        'template': 'dashboard.html',
+        'extra_context': {
+            'stats': stats,
+        },
+    })
+
     return direct_to_template(request, *args, **kwargs)
 
 @login_required
