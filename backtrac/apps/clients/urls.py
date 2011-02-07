88f36bc9de6e76760c53410bff01a1bfe7281eee
Rob Golding <rob@robgolding.com>
2011-02-07 22:40:22 +0000

    Change client list/detail view to class-based generics

--- backtrac/apps/clients/urls.py
+++ backtrac/apps/clients/urls.py
@@ -1,36 +1,36 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list_detail import object_list, object_detail
+from django.views.generic import DetailView, ListView
 
 from models import Client
 import views
 
 urlpatterns = patterns('',
 
-    url(r'^$', login_required(object_list), {
-                    'template_name': 'clients/client_list.html',
-                        'queryset': Client.objects.select_related()},
-                    name='clients_client_list'),
+    url(r'^$', login_required(ListView.as_view(
+        template_name='clients/client_list.html',
+        queryset=Client.objects.select_related(),
+    )), name='clients_client_list'),
 
-    url(r'^(?P<object_id>\d+)/$',
-                    login_required(object_detail), {
-                        'template_name': 'clients/client_detail.html',
-                        'queryset': Client.objects.select_related()},
-                    name='clients_client_detail'),
+    url(r'^(?P<pk>\d+)/$', login_required(DetailView.as_view(
+        template_name='clients/client_detail.html',
+        queryset=Client.objects.select_related(),
+    )), name='clients_client_detail'),
 
     url(r'^(?P<client_id>\d+)/update/$', views.update_client,
-                    name='clients_update_client'),
+        name='clients_update_client'),
 
     url(r'^(?P<client_id>\d+)/pause/$', views.pause_client,
-                    name='clients_pause_client'),
+        name='clients_pause_client'),
 
     url(r'^(?P<client_id>\d+)/resume/$', views.resume_client,
-                    name='clients_resume_client'),
+        name='clients_resume_client'),
 
     url(r'^(?P<client_id>\d+)/delete/$', views.delete_client,
-                    name='clients_delete_client'),
+        name='clients_delete_client'),
 
-    url(r'^create/$', views.create_client,
-                    name='clients_create_client'),
+    url(r'^create/$', views.CreateClientView.as_view(),
+        name='clients_create_client'),
 
 )
