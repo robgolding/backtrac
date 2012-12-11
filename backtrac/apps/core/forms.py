a155888a842e1f750ed5581f73cb4e1d13ad9112
Rob Golding <rob@robgolding.com>
2012-12-11 18:06:34 +0000

    Add extra exclusion to config view by default

--- backtrac/apps/core/forms.py
+++ backtrac/apps/core/forms.py
@@ -4,4 +4,4 @@ from django.forms.formsets import BaseFormSet, formset_factory
 class ExclusionForm(forms.Form):
     glob = forms.CharField(max_length=255, required=False)
 
-ExclusionFormSet = formset_factory(ExclusionForm, formset=BaseFormSet, extra=0)
+ExclusionFormSet = formset_factory(ExclusionForm, formset=BaseFormSet, extra=1)
